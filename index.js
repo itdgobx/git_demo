98c521d1500478a51ca34b08711407fe6697cae8
KAI LIU <itdgobx@gmail.com>
2024-09-26 20:21:34 +1000

    feat: add about us

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', function(req,res){
+    res.send('hello world')
+})
+
+app.get('/about', function(req,res){
+    res.send('About Us')
+})
 app.listen(8080, ()=>{
     console.log('Listening at port 8080.')
 })
